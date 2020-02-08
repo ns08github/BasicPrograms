4ef5fe0cd60ace2dba2dff868b82803dbac35f0e
mohammad_rizvi_hashedin <Mohammad.Rizvi.hashedin@cohesity.com>
2020-02-09 01:56:02 +0530

    rearrange folder structure

--- ProblemsSolving/src/String/FirstNonRepeatingCharacter.java
+++ ProblemsSolving/src/String/FirstNonRepeatingCharacter.java
@@ -1,4 +1,4 @@
-package String.RemoveRepetativeCharacter;
+package String;
 
 import java.util.HashMap;
 import java.util.Scanner;
--- ProblemsSolving/src/String/RemoveRepetitiveCharacter.java
+++ ProblemsSolving/src/String/RemoveRepetitiveCharacter.java
@@ -1,4 +1,4 @@
-package String.RemoveRepetativeCharacter;
+package String;
 
 
 import org.jetbrains.annotations.NotNull;
